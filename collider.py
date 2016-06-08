5d0e6aaf092469b91b43d892ab82ea1ce7c89643
Ddedalus <h.beres@hotmail.com>
2016-06-09 00:11:45 +0200

    Critical error found in B definition
    Bug fixes

--- collider.py
+++ collider.py
@@ -1,7 +1,7 @@
 import numpy as np
 from Body import *
 
-bill = Body(1, 1, x=0, y=0, vx=1, vy=0)
+bill = Body(1, 1, x=0, y=0, vx=0, vy=1)
 ring = Body(1, 4, x=0, y=0, vx=0, vy=0)
 pos_b = [np.array([bill.x, bill.y])]
 
@@ -10,11 +10,11 @@ def collide(b, r):
 	# mas, radius, x, y, vx, vy
 
 	A = b.v_sq() + r.v_sq() - 2 * (b.vx * r.vx + b.vy * r.vy)
-	B = (b.x + r.x) * (b.vx + r.vx) + (b.y + r.y) * (b.vy + r.vy)
+	B = 2 * ((b.x - r.x) * (b.vx - r.vx) + (b.y - r.y) * (b.vy - r.vy))
 	C = (b.x - r.x)**2 + (b.y - r.y)**2 - (b.r - r.r)**2
 	D = B**2 - 4 * A * C        # discriminant of L**2 = (b.r - r.r)**2
 
-	if D <= 0:
+	if D < 0:
 		print("Negative discriminant:", D, "no ongoing collision...")
 		return 1
 
@@ -25,8 +25,8 @@ def collide(b, r):
 	for k in [b, r]:
 		k.new_x, k.new_y = k.x + t * k.vx, k.y + t * k.vy
 
-	sin_f = (b.new_y - r.new_y) / np.sqrt(len_sq(b, r, new=True))
-	cos_f = (b.new_x - r.new_x) / np.sqrt(len_sq(b, r, new=True))
+	sin_f = (b.new_x - r.new_x) / np.sqrt(len_sq(b, r, new=True))
+	cos_f = (b.new_y - r.new_y) / np.sqrt(len_sq(b, r, new=True))
 	# angle between x-y and d-e coordinates
 	# d is tangent to both circles in point of collision
 
@@ -35,8 +35,8 @@ def collide(b, r):
 		k.ve = k.vx * sin_f + k.vy * cos_f
 	# velocities in new coordinates
 
-	b.we = (b.ve * (r.m - b.m) + 2 * r.m * b.ve) / (b.m + r.m)
-	r.we = (r.ve * (b.m - r.m) + 2 * b.m * r.ve) / (b.m + r.m)
+	b.we = (b.ve * (r.m - b.m) + 2 * r.m * r.ve) / (b.m + r.m)
+	r.we = (r.ve * (b.m - r.m) + 2 * b.m * b.ve) / (b.m + r.m)
 
 	for k in [b, r]:
 		k.new_vx = k.vd * cos_f + k.we * sin_f
@@ -44,6 +44,8 @@ def collide(b, r):
 
 	pos = b.pos()
 	b.step()
+	b.we = b.vd = b.ve = None
+	r.we = r.vd = r.ve = None
 	r.step()
 	return pos
 

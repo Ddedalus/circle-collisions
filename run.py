874814beda385586d7d6c3ef94900c1879aa8d92
Ddedalus <h.beres@hotmail.com>
2016-06-13 00:42:09 +0200

    Introduced ploter.py

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from collider import *
 
-bill = Body(1, 1, [0, 0], [1, 1])
-ring = Body(2, 4, [0, 0], [0, 0])
+bill = Body(1, 1, [0.5, 0], [1, 1])
+ring = Body(6.5, 4, [0, 0], [0, 0])
 pos_b = [bill.pos]
 
 switch_to_masspoint(bill, ring)
@@ -9,4 +9,5 @@ print('Pęd układu:', summary_momentum(bill, ring))
 
 for i in range(10):
 	pos_b.append(collide(bill, ring))
-	print(pos_b[-1])
+	# print(pos_b[-1])
+	print ('Radius:', np.linalg.norm(bill.v))

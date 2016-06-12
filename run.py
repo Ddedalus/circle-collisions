4195490e5f3a135319b3767909951c2824d8fdca
Ddedalus <h.beres@hotmail.com>
2016-06-13 00:12:48 +0200

    Solved non-zero momentum bug

--- run.py
+++ run.py
@@ -1,10 +1,16 @@
 from collider import *
+from numpy.linalg import norm
 
 
 bill = Body(1, 1, [0, 0], [1, 1])
-ring = Body(1, 4, [0, 0], [0, 0])
+ring = Body(2, 4, [0, 0], [0, 0])
 pos_b = [bill.pos]
+print('Pęd początkowy:', bill.v * bill.m + ring.v * ring.m)
 
-for i in range(50):
+switch_to_masspoint(bill, ring)
+print('Pęd masspoint:', bill.v * bill.m + ring.v * ring.m)
+
+for i in range(10):
 	pos_b.append(collide(bill, ring))
-	print(pos_b[-1])
+	# print(pos_b[-1])
+	print('Pęd', i+1, ':', bill.v * bill.m + ring.v * ring.m)
